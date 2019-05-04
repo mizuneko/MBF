ff2ee565fc76196872a4d8b73ad60eab2373a850
Kenji Tabata <retmiz@gmail.com>
2019-05-04 13:49:53 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using MicroBatchFramework;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace MBF
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await new HostBuilder().RunBatchEngineAsync<MyFirstBatch>(args);
+        }
+    }
+
+    public class MyFirstBatch : BatchBase
+    {
+        public void Hello(string name, int repeat = 3)
+        {
+            for (int i = 0; i < repeat; i++)
+            {
+                this.Context.Logger.LogInformation($"Hello My Batch from {name}");
+            }
+        }
+    }
+}
